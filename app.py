31ae87f8bd4acebad62cddb726af827a2c6e97ce
William Hinkley <wihi1131@colorado.edu>
2023-11-08 04:10:10 +0000

    Commit 1 - connected to postgresql database

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import psycopg2
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/db_test')
+def testing():
+    conn = psycopg2.connect("postgres://lab_10_user:ywXp3cXmRpClRAPlpfr5SpcgEfZUnBNN@dpg-cl5gifl6fh7c73emv5i0-a/lab_10")
+    conn.close()
+    return "Database Connection Successful!"
